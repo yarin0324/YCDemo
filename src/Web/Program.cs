f9ae82339a64f6e26c786ffacd3d0ab59bc88d46
yarin_yang <yarin_yang@gss.com.tw>
2024-04-04 10:15:56 +0800

    feat: 建立第一個頁面 調整項目: 1. net 6路由設定 2. 新增controller與view

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,9 +1,21 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+// Using a custom DI container.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(startup.ConfigureContainer);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,5 +29,9 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure;
+﻿using Autofac;
+using Infrastructure;
 
 namespace Web
 {
@@ -13,24 +14,45 @@ namespace Web
 
         public void ConfigureServices(IServiceCollection services)
         {
-            #region 註冊 Configuration
+            //#region 註冊 Configuration
 
-            services.AddSingleton(Configuration);
+            //services.AddSingleton(Configuration);
 
-            #endregion
+            //#endregion
             
-            #region 解析 DbSettings
+            //#region 解析 DbSettings
 
-            services.Configure<DbSettings>(Configuration.GetSection("DbSettings"));
+            //services.Configure<DbSettings>(Configuration.GetSection("DbSettings"));
             
-            // 注入經橋接後被解析的 DbSettings
-            services.AddScoped<IDbSettingsResolved, DbSettingsBridge>();
+            //// 注入經橋接後被解析的 DbSettings
+            //services.AddScoped<IDbSettingsResolved, DbSettingsBridge>();
 
-            #endregion
+            //#endregion
+
+            services.AddRazorPages();
+        }
+
+        //  Using a custom DI container
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            // Configure custom container.
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
         }
     }
 }
